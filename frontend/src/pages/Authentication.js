7ac55f65f936a365a073dda6f4661af95c705186
luidimso <luidi_mso@outlook.com>
2023-11-30 10:03:05 -0300

    Implementing authentication action and response validation

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { json, redirect } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -5,3 +6,33 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+export async function action({request}) {
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password")
+  };
+
+  const response = await fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(authData)
+  });
+
+  if(response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if(!response.ok) {
+    throw json({
+      message: "Could not athenticate User. Internal server erro.",
+      status: 500
+    })
+  }
+
+  // TO DO
+  return redirect("/");
+}
