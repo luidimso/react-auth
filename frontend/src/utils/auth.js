f2a2808ddc95d16b5ce2e15c5b06d06ccc62f19b
luidimso <luidi_mso@outlook.com>
2023-11-30 14:37:11 -0300

    Using route prop loader to implement route guard checking if there is auth token stored

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from "react-router-dom";
+
 export function getAuthToken() {
     const token = localStorage.getItem("token");
     return token;
@@ -5,4 +7,15 @@ export function getAuthToken() {
 
 export function tokenLoader() {
     return getAuthToken();
+}
+
+export function checkAuthLoader() {
+    const token = getAuthToken();
+
+    if(!token) {
+        alert("You should authenticate to access this page")
+        return redirect("/auth");
+    }
+
+    return null;
 }
